3f19c9aca5da489b78c13cd64dc9d39ff9d3082b
yewonkim01 <kyw9791@naver.com>
2024-02-25 01:52:05 +0900

    app.py revised

--- app.py
+++ app.py
@@ -7,10 +7,13 @@ from DB_class import DB
 from submit_df import submit_df
 from email_info import email_set, data
 
+print('page reloaded')
+
 
 #이메일로 로그인
 def login_email() -> tuple:
     email = st.text_input("본인의 이메일로 로그인하세요.")
+    email = email.strip()
 
     if email in email_set:
         name = data.get(email)
@@ -54,15 +57,15 @@ def all(qs:list, As:list, chapter:str, chapter_name:str, name:str, email:str):
     st.subheader(f"[Chapter{chapter[2:]}] {chapter_name}")
 
     #문제들을 tab으로 구현
-    tab1, tab2, tab3, tab4, tab5 = st.tabs([f'Q{i}' for i in range(1, len(qs)+1)])
-    tabs = [tab1, tab2, tab3, tab4, tab5]
+    tabs = st.tabs([f'Q{i}' for i in range(1, len(qs)+1)])
+    #tabs = [tab1, tab2, tab3, tab4, tab5]
 
     db = DB(chapter, name)
     
     #존재하는 tab수만큼 반복문 돌리면서 화면 구성
-    for i in range(len(tabs)):
+    for i, tab in enumerate(tabs):
 
-        with tabs[i]:
+        with tab:
             col1, col2 = st.columns(2)  #col1은 왼쪽 문제보이는 열, col2는 오른쪽 답변과 정답확인하는 열 
 
             #왼쪽 문제 열
@@ -92,35 +95,31 @@ def all(qs:list, As:list, chapter:str, chapter_name:str, name:str, email:str):
             with col2:
                 st.write('\n')
 
-                #이미 제출한 답변이 있으면 기본값(value)으로 답변작성란에 띄워지도록
                 submitted_ans = db.submitted_answer(i)
-                
-                #제출한 답변 있으면
                 if submitted_ans:
                     value = submitted_ans
-                #제출한 답변 없으면 ""(빈칸)
                 else:
                     value = ""
-                answer = st.text_area("", key = f"ans{i+1}", height=200, placeholder="답안을 작성해 주세요.", value = value)
-
-                #제출하기 button
-                button = st.button("제출하기", key=f"button{i+1}")
+                answer = st.text_area(" ", key = f"ans{i+1}", height=200, placeholder="답안을 작성해 주세요.", value = value)
 
                 #제출된 답변 있으면 정답화면 바로 보이게/
                 submits = db.submitted_check(qs) #submits = [1,0,1,1,1]
+
+                button = st.button("제출하기", key=f"button{i+1}", disabled= (len(answer.strip()) < 10))
                 if submits[i]:
                     st.markdown(' :green[☑ 제출되었습니다.]')
                     show_ans(As[i])
 
                 #제출된 답변 없으면 <제출하기> 버튼 눌러야 정답확인 가능 
                 if len(answer.strip()) >= 10:   #10글자 이상 작성
-
                     if button:
+                        print(answer)
                         #이미 제출했는데 버튼 또 누르면(재제출)
                         if submits[i]:
                             #st.balloons() 풍선나옴!!
                             #db에만 답변 저장
                             db.save_db(i+1, answer)
+                            st.rerun()
                             
                         #처음 제출이면
                         else:
@@ -128,10 +127,12 @@ def all(qs:list, As:list, chapter:str, chapter_name:str, name:str, email:str):
                             #제출문구 띄우고 답변 보여주기
                             st.markdown(' :green[☑ 제출되었습니다.]')
                             show_ans(As[i])
+                            st.rerun()
+                            
                         q,w,e,r,t = st.columns(5)
                         with t:
                             st.button("다음 문제로 이동>") #이거 작동안함 / tab으로 구현했는데 어떻게 연결할지 모르겠음
-                        
+                            
                 else:
                     st.markdown('10자 이상 작성해주세요.')
                 
