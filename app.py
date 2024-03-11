630923f152d2fc300171790b8054c851c7397ae1
Yewon Kim <115199510+yewonkim01@users.noreply.github.com>
2024-03-11 14:01:19 +0900

    Update app.py
    Signed-off-by: Yewon Kim <115199510+yewonkim01@users.noreply.github.com>

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def show_answer(A:str):
     st.markdown(f'<div class="ans-rounded-box">{A}</div>', unsafe_allow_html=True)
 
 
-def all(db, deadline:str, Qs:list, As:list, chapter:str, chapter_name:str, name:str, email:str):
+def all(col2, db, deadline:str, Qs:list, As:list, chapter:str, chapter_name:str, name:str, email:str):
     '''
     db: 데이터베이스
     Qs: 해당 주차 문제 담긴 리스트
@@ -55,8 +55,15 @@ def all(db, deadline:str, Qs:list, As:list, chapter:str, chapter_name:str, name:
     email: 학회원 이메일
     '''
 
+    #상단 오른쪽에 제출했는지 데이터프레임 보여주기
+    with col2:
+        a,b = st.columns(2)
+        with b:
+            db.submit_df(chapter, name, len(Qs))
+
     # 페이지 서브헤더 제목 설정
     st.subheader(f"[Chapter{chapter[2:]}] {chapter_name}")
+    
 
     #문제들을 tab으로 구현
     tabs = st.tabs([f'Q{i}' for i in range(1, len(Qs)+1)])
@@ -122,7 +129,7 @@ def all(db, deadline:str, Qs:list, As:list, chapter:str, chapter_name:str, name:
                         if submits[i]:
                             # db에만 답변 저장
                             db.save_db(i+1, answer)
-                            #st.rerun()
+                            st.rerun()
                             
                         # # 처음 제출이면
                         else:
@@ -130,7 +137,7 @@ def all(db, deadline:str, Qs:list, As:list, chapter:str, chapter_name:str, name:
                             # 제출문구 띄우고 답변 보여주기
                             with d: st.markdown(' :green[☑ 제출되었습니다.]')
                             show_answer(As[i])
-                            #st.rerun()
+                            st.rerun()
                 
           
 if __name__ == "__main__":
@@ -222,13 +229,9 @@ if __name__ == "__main__":
 
         db = DB(chapter, name)
         
-        all(db, deadline, Qs, As, chapter, chapter_name, name, email)
+        all(col2, db, deadline, Qs, As, chapter, chapter_name, name, email)
 
-        #상단 오른쪽에 제출했는지 데이터프레임 보여주기
-        with col2:
-            a,b = st.columns(2)
-            with b:
-                db.submit_df(chapter, name, len(Qs))
+        
 
 
                 
